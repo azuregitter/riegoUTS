ee2e19f60920d846e43f75a3e349db9dc8545184
azuregitter <geromeriego@gmail.com>
2020-10-21 23:14:44 +0800

    initial commit

---
+++ precache-manifest.37811b9f16888d30b4681b89974c05df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc5ad07afb654715f584120c8d6306c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5f101625f505deb1153",
+    "url": "/static/css/main.cd43039f.chunk.css"
+  },
+  {
+    "revision": "536d79bcede55d5f60c7",
+    "url": "/static/js/2.2e2c4eef.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.2e2c4eef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5f101625f505deb1153",
+    "url": "/static/js/main.69a52328.chunk.js"
+  },
+  {
+    "revision": "e69f43daa6c8535ae40b",
+    "url": "/static/js/runtime-main.20a88221.js"
+  }
+]);
